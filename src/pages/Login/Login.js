f49888e90fab5c313e6efe391f2b62a75bb54868
ssusa2 <audgns9207@naver.com>
2022-03-02 13:57:44 +0900

    Add: Login 컴포넌트 스타일링 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,25 @@
+import './Login.scss';
+
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div className="Login">
+      <h2 className="title">회원 로그인</h2>
+      <div className="loginBox">
+        <form>
+          <input className="inputText" placeholder="아이디" />
+          <input className="inputText" placeholder="비밀번호" type="password" />
+          <div className="check">
+            <input className="inputCheck" type="checkbox" id="c_box" />
+            <label for="c_box"></label>
+            <label for="c_box" className="checkText">
+              아이디 저장
+            </label>
+          </div>
+        </form>
+        <button className="loginBtn">로그인</button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;

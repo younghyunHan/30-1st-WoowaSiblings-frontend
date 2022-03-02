d0cdbb1e6dd9d4e0675a07845b94660608b2188c
ssusa2 <audgns9207@naver.com>
2022-03-02 19:51:20 +0900

    Modify: 리뷰반영(font-> import, 컨벤션 수정, a태그 href값 수정)

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,22 +5,22 @@ function Nav() {
     <nav className="Nav">
       <div className="topBar" />
       <div className="navWarp">
-        <div className="top">
-          <ul className="top-header">
+        <div className="userMenu">
+          <ul className="userMenuText">
             <li>
-              <a href="#">로그인</a>
+              <a href="/">로그인</a>
             </li>
-            <span className="txt_bar" />
+            <span className="txtBar" />
             <li>
-              <a href="#">회원가입</a>
+              <a href="/sign">회원가입</a>
             </li>
-            <span className="txt_bar" />
+            <span className="txtBar" />
             <li>
-              <a href="#">마이페이지</a>
+              <a href="/mypage">마이페이지</a>
             </li>
-            <span className="txt_bar" />
+            <span className="txtBar" />
             <li>
-              <a href="#">장바구니</a>
+              <a href="/cart">장바구니</a>
             </li>
             <a className="cartNumber" href="#">
               0
@@ -29,7 +29,7 @@ function Nav() {
         </div>
 
         <div className="middle">
-          <a href="#">
+          <a href="/main">
             <img className="logo" alt="배민문방구" src="images/welogo.png" />
           </a>
           <form className="search">
@@ -43,23 +43,22 @@ function Nav() {
         <div className="cate">
           <ul>
             <li>
-              <a href="#">전체</a>
+              <a href="/all">전체</a>
             </li>
             <li>
-              <a href="#">삼십기스</a>
+              <a href="/goods_service">삼십기스</a>
             </li>
             <li>
-              <a href="#">30기의 물건</a>
+              <a href="/goods_things">30기의 물건</a>
             </li>
             <li>
-              <a href="#">멘톳쓰</a>
+              <a href="/goods_mentors">멘톳쓰</a>
             </li>
             <li>
-              <a href="#">삼십세끼</a>
+              <a href="/goods_eat">삼십세끼</a>
             </li>
           </ul>
         </div>
-        <div className="bottomLine"></div>
       </div>
     </nav>
   );
---
+++ src/pages/Sign/Sign.js
@@ -0,0 +1,11 @@
+function Sign() {
+  return (
+    <div className="Sign">
+      <h2>기본정보</h2>
+      <p>* 표시는 반드시 입력하셔야 하는 항목입니다.</p>
+      <div className="signTalble"></div>
+    </div>
+  );
+}
+
+export default Sign;

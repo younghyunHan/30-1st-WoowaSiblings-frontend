74daefe56bc44f2b9e299d10be27ee9bb81c01ab
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-03-03 21:26:08 +0900

    Fix: footer.js 반복되는 부분 map으로 구현

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import footerInfoData from './FooterInfoData';
+import footerDataList from './FooterDataList';
 import './Footer.scss';
 
 function Footer() {
@@ -13,37 +16,22 @@ function Footer() {
         </div>
         <div className="footerContent">
           <ul className="footerInfo">
-            <a href="#" className="footerInfoList">
-              공지사항
-            </a>
-            <a href="#" className="footerInfoList">
-              1:1문의
-            </a>
-            <a href="#" className="footerInfoList">
-              이용약관
-            </a>
-            <a href="#" className="footerInfoList">
-              개인정보처리방침
-            </a>
-            <a href="#" className="footerInfoList">
-              판매처 안내
-            </a>
+            {footerInfoData.map(info => {
+              return (
+                <a href="#" className="footerInfoList" key={info.id}>
+                  {info.content}
+                </a>
+              );
+            })}
           </ul>
           <div className="footerData">
-            <div className="footerDataList">
-              상호: (주)우아한남매들 | 대표 : siblings | 사업자등록번호 :
-              000-00-0000 통신판매업신고번호 : 2999-서울선릉-1002
-            </div>
-            <div className="footerDataList">
-              팩스번호: 050-123-3412 | 메일 : 123@siblings.com | 삼십 문방구 :
-              @siblings_store
-            </div>
-            <div className="footerDataList">
-              주소 : 서울특별시 선릉 테헤란로 427
-            </div>
-            <p className="footerDataList">
-              © Woowa Siblings Corp. All right Reserved
-            </p>
+            {footerDataList.map(data => {
+              return (
+                <li className="footerDataList" key={data.id}>
+                  {data.content}
+                </li>
+              );
+            })}
           </div>
         </div>
       </div>
---
+++ src/components/Footer/FooterInfoData.js
@@ -0,0 +1,24 @@
+let FOOTER_INFO_DATA = [
+  {
+    id: 1,
+    content: '공지사항',
+  },
+  {
+    id: 2,
+    content: '1:1문의',
+  },
+  {
+    id: 3,
+    content: '이용약관',
+  },
+  {
+    id: 4,
+    content: '개인정보처리방침',
+  },
+  {
+    id: 5,
+    content: '판매처 안내',
+  },
+];
+
+export default FOOTER_INFO_DATA;

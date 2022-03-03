41acb55ec2a38d5b5e33b8ec4c04bf654156b103
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-03-03 15:26:00 +0900

    Add: main 페이지 product 리스트 레이아웃 작업 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,57 @@
+import { useState, useEffect } from 'react';
+import './Main.scss';
+
 function Main() {
-  return <h1>main</h1>;
+  const [mainList, setMainList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/mainData.json')
+      .then(res => res.json())
+      .then(data => {
+        setMainList(data);
+      });
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="mainListMainWrap">
+        <div className="mainListContent">
+          <ul className="mainListCotentGallery">
+            {mainList.map(content => {
+              return (
+                <li key={content.id} className="listGallery">
+                  <div className="GalleryItem">
+                    <div className="itemPhotoBox">
+                      <a href="#" className="itemPageMove">
+                        <img
+                          className="mainImage"
+                          src={content.image}
+                          alt="goodsItem"
+                        />
+                      </a>
+                    </div>
+
+                    <div className="itemDiscountRate">
+                      {content.discountRate}
+                    </div>
+                    <div className="itemTitleBox">
+                      <a href="#" className="itemTitleBoxName">
+                        <span>{content.itemName}</span>
+                      </a>
+                    </div>
+                    <div className="itemMoneyBox">
+                      <span className="originalPrice">49,000원</span>
+                      <span className="discountPrice">29,000원</span>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;

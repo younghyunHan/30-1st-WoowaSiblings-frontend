e3cdc982f61d3c22c798b17a7f723725d0e17419
ssusa2 <audgns9207@naver.com>
2022-03-04 13:57:47 +0900

    Add: 회원가입 기능 구현중..

--- src/pages/Sign/Sign.js
+++ src/pages/Sign/Sign.js
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+import './Sign.scss';
 function Sign() {
-  <h1>Sign</h1>;
+  const [values, setValues] = useState({ id: '', password: '' });
+
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const goToMain = () => {
+    fetch('http://10.58.2.78:8000/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: values.id,
+        password: values.password,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.ACCESS_TOKEN) {
+          navigate('/');
+        } else if (data.message === 'INVALID_USER') {
+          alert('유효한 id입니다.');
+        }
+      });
+  };
+
+  return (
+    <div className="login">
+      <h2 className="title">회원 가입</h2>
+      <div className="loginBox">
+        <form>
+          <input
+            onChange={handleChange}
+            name="id"
+            className="inputText"
+            placeholder="아이디"
+          />
+          <input
+            onChange={handleChange}
+            name="password"
+            className="inputText"
+            placeholder="비밀번호"
+            type="password"
+          />
+        </form>
+        <button onClick={goToMain} className="loginBtn">
+          회원가입
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Sign;

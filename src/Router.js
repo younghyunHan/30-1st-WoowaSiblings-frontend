94647a2e99abc1825b2bc7678e9d7e869cf00473
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-03-06 19:33:50 +0900

    Add: 상세페이지 구현중

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import Cart from './pages/Cart/Cart';
 import Goods from './pages/Goods/Goods';
+import ItemDetail from './pages/Goods/ItemDetail';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 
@@ -17,6 +18,7 @@ function Router() {
         <Route path="/main" element={<Main />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/goods" element={<Goods />} />
+        <Route path="/itemDetail" element={<ItemDetail />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/Goods/ItemDetail.js
@@ -0,0 +1,56 @@
+import './ItemDetail.scss';
+
+function ItemDetail() {
+  return (
+    <div className="itemDetail">
+      <div className="itemDetailContents">
+        <img
+          className="itemDetailImage"
+          src="https://images.unsplash.com/photo-1644221876015-b8bbd9e1f89f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+          alt="상세이미지"
+        />
+        <div className="itemInfoBox">
+          <h3 className="itemDetailTitle">배달이 친구들</h3>
+          <div className="itemDetailList">
+            <div className="itemPrice">
+              <div className="itemPriceOne">판매가격 </div>
+              <span className="itemPriceTwo">3000원</span>
+            </div>
+            <div className="itemDelivery">
+              <div className="itemDeliveryOne">배송정보 </div>
+              <span className="itemDeliveryTwo">
+                배송이 힘들어요... 찾으러 오세요
+              </span>
+            </div>
+            <div className="itemOptionBox">
+              <div className="itemOptionBoxOne">옵션</div>
+              <select className="itemOptionBoxTwo">
+                <option value="">= 옵션 : 가격 =</option>
+                <option value="">독고배달이</option>
+                <option value="">냥이배달이</option>
+                <option value="">메이배달이</option>
+                <option value="">왕배달이</option>
+              </select>
+            </div>
+          </div>
+          <div className="totalAmount">
+            <div>총 합계금액</div>
+            <div className="totalPrice">
+              <strong>3000원</strong>
+            </div>
+          </div>
+          <div className="btnChoiceBox">
+            <button type="button" className="btnAddCart">
+              장바구니
+            </button>
+            <button type="button" className="btnAddOrder">
+              바로구매
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemDetail;

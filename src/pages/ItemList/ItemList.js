86b156835a30b8e65e252725e825446844d828d4
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-03-06 14:33:36 +0900

    - Add : 디자인 수정  - padding top, padding bottom 수정  - 가격 디자인 수정

--- src/pages/ItemList/ItemList.js
+++ src/pages/ItemList/ItemList.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import { useState, useEffect } from 'react';
 import './ItemList.scss';
 
@@ -5,51 +6,58 @@ function ItemList() {
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    fetch('/data/listData.json')
+    fetch('http://10.58.0.75:8000/products/productlist')
       .then(res => res.json())
       .then(data => {
-        setList(data);
+        setList(data.results);
       });
   }, []);
-
   return (
     <div className="itemList">
-      <div className="itemListWrap">
-        <div className="itemListContent">
-          <ul className="itemListCotentGallery">
-            {list.map(content => {
-              return (
-                <li key={content.id} className="listGallery">
-                  <div className="galleryItem">
-                    <div className="itemPhotoBox">
-                      <a href="#" className="itemPageMove">
-                        <img
-                          className="listImage"
-                          src={content.image}
-                          alt="listItem"
-                        />
-                      </a>
-                    </div>
-                    <div className="itemDiscountRate">
-                      {content.discountRate}
-                    </div>
-                    <div className="itemTitleBox">
-                      <a href="#" className="itemTitleBoxName">
-                        <span>{content.itemName}</span>
-                      </a>
-                    </div>
-                    <div className="itemMoneyBox">
-                      <span className="originalPrice">{content.itemPrice}</span>
-                      <span className="discountPrice">
-                        {content.itemDiscountPrice}
-                      </span>
-                    </div>
+      <div className="itemListContent">
+        <ul className="itemListCotentGallery">
+          {list.map(content => {
+            return (
+              <li key={content.id} className="listGallery">
+                <div className="galleryItem">
+                  <div className="itemPhotoBox">
+                    <a href="#" className="itemPageMove">
+                      <img
+                        className="listImage"
+                        src={content.thumbnail_image}
+                        alt="listItem"
+                      />
+                    </a>
                   </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                  <div className="itemDiscountRate">
+                    {content.discount_rate === ''
+                      ? ''
+                      : `${content.discount_rate * 100}%`}
+                  </div>
+                  <div className="itemTitleBox">
+                    <a href="#" className="itemTitleBoxName">
+                      <span>{content.name}</span>
+                    </a>
+                  </div>
+                  <div className="itemMoneyBox">
+                    <span
+                      className={`originalPrice ${
+                        content.discount_rate ? '' : 'priceBig'
+                      }`}
+                    >
+                      {content.price}원
+                    </span>
+                    <span className="discountPrice">
+                      {content.discount_rate
+                        ? `${content.discount_price}원`
+                        : ''}
+                    </span>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
